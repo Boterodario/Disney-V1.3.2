a770a1e292f54add4bb03c218b21203ba5514a72
Boterodario <dario.botero@hotmail.com>
2023-01-14 21:59:45 -0500

    modificaciones

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-window.addEventListener("scroll" , function (){
-    var body = document.querySelector("body");
-    body.classList.toggle("down" , window.scrollY>0);
-})
+// const coll = document.getElementsByClassName('collapsible');
+
+// for (i=0; i<coll.length; i++) {
+// coll[i].addEventListener('click', function () {
+    // // console.log("hola");
+    // this.classList.toggle('active')
+// })
+// }
+
+
+
+
+
+
+
+
+ var coll = document.getElementsByClassName("collapsible");
+ var i;
+
+ for (i = 0; i < coll.length; i++) {
+   coll[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var content = this.nextElementSibling;
+   if (content.style.maxHeight){
+     content.style.maxHeight = null;
+   } else {
+     content.style.maxHeight = content.scrollHeight + "px";
+   } 
+   });
+ }
