97a89774fc1689f70c4962ebefe4c5ed1fe214ad
Boterodario <dario.botero@hotmail.com>
2023-01-23 11:19:50 -0500

    se modifica el menu

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 
-let = ubicacionPrincipal =window.pageYOffset;
-window.onscroll = function() {
-  let = Desplazamiento__Actual = window.pageYOffset;
-  if(ubicacionPrincipal >= Desplazamiento__Actual){
-    document.toggle("nav__click--item").style.top = "0";
-  }
-  else{
-    document.toggle("nav__click--item").style.top = "-600px";
-  }
-  ubicacionPrincipal = Desplazamiento__Actual
-}
-
-
-
-
- var coll = document.getElementsByClassName("collapsible");
- var i;
-
- for (i = 0; i < coll.length; i++) {
-   coll[i].addEventListener("click", function() {
-   this.classList.toggle("active");
-   var content = this.nextElementSibling;
-   if (content.style.maxHeight){
-     content.style.maxHeight = null;
-   } else {
-     content.style.maxHeight = content.scrollHeight + "px";
-   } 
-   });
+ let = ubicacionPrincipal =window.pageYOffset;
+ window.onscroll = function() {
+ let = Desplazamiento__Actual = window.pageYOffset;
+ if( ubicacionPrincipal >= Desplazamiento__Actual){
+  document.getElementById("nav").style.display = "0";
  }
+ else{
+ document.getElementById("nav").style.display= "-100px";
+ }
+ ubicacionPrincipal = Desplazamiento__Actual
+ }
+
+
+
+
+
+
+
+ function changeBackground() {
+ // document.getElementById('nav').style.backgroundColor = color;
+ document.getElementById("nav").style.justifyContent = "space-around";
+ document.getElementById("nav").style.display = "none";
+ document.getElementById("nav__item1").style.display = "block";
+ document.getElementById("nav__item2").style.display = "block";
+ document.getElementById("nav__item3").style.display = "block";
+}
+ window.addEventListener("scroll",function() { changeBackground() });
+
+
+
+
+
+
+
+
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
