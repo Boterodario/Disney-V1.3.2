d7348bd997368280ebb2a6ed612246f477fa8594
Boterodario <dario.botero@hotmail.com>
2023-01-30 16:47:17 -0500

    se realiza parte faltante menu

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
 
-var  prevScrollpos =window.pageYOffset;
-window.onscroll = function() {
-var   currentScrollPos  = window.pageYOffset;
-if( prevScrollpos > currentScrollPos){
-  document.getElementById("nav").style.top = "0";
-  document.getElementById("nav__menu").style.display = "none";
+
+//MENU CON SCROLL
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+    document.getElementById("nav__menu").style.display = "none";
+  }
+  else {
+    document.getElementById("nav").style.top = "-600px";
+    document.getElementById("nav__menu").style.display = "block";
+
+  }
+  prevScrollpos = currentScrollPos
 }
-else{
-  document.getElementById("nav").style.top = "-600px";
-  document.getElementById("nav__menu").style.display = "block";
-  
-}
-prevScrollpos = currentScrollPos
-}
-
-// var fax = window.pageYOffset;
-  // window.onscroll= function (){
-    // var superfax = window.pageYOffset;
-    // if ( fax > superfax){
-      // document.getElementById("nav__menu").style.display = "none"
-    // }else{
-      // document.getElementById("nav__menu").style.display = "block"
-    // }
-    // fax = superfax
-// }
-
-//  function changeBackground() {
-  // var scrollTop = window.scrollY
-  // if(scrollTop < 500){
-  // document.getElementById("nav").style.display = "none";
-  // document.getElementById("nav__menu").style.display = "block";
-  // }
-  // window.addEventListener("scroll",function() { changeBackground() });
-//  }
-
- // document.getElementById('nav').style.backgroundColor = color;
-//  document.getElementById("nav__menu").style.justifyContent = "space-around";
-//  document.getElementById("nav__menu").style.display = "block";
-//  document.getElementById("nav").style.display = "none";
 
 
-
- 
-// }
-//  window.addEventListener("scroll",function() { changeBackground() });
-
-
-
-
-
-
-
-
-
+//MENU DESPLEGABLE
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
